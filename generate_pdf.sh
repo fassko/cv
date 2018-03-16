85dc653d87db3b3365354a9d6ffb4e23979ba782
Kristaps Grinbergs <fassko@gmail.com>
2018-03-17 01:16:36 +0200

    Add travis build

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -2,12 +2,12 @@
 
 npm install mdpdf -g
 mdpdf README.md CV-Kristaps-Grinbergs.pdf
-if [ "$TRAVIS" = "true" ]
-then
-  echo "Don't push on CI"
-else
+# if [ "$TRAVIS" = "true" ]
+# then
+#   echo "Don't push on CI"
+# else
   DATE=`date "+%d.%m.%Y %H %H:%M:%S"`
   git commit CV-Kristaps-Grinbergs.pdf -m "Generated PDF ${DATE}"
   git push
   echo "Pushed new PDF"
-fi
+# fi
